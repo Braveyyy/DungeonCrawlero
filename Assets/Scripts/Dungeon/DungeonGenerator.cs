ae497890edcbba8c333b41b4f51ae50577f860ba
Adam <latifasedyeame@gmail.com>
2024-11-02 11:18:22 -0600

    edit DungeonGenerator.cs

--- Assets/Scripts/Dungeon/DungeonGenerator.cs
+++ Assets/Scripts/Dungeon/DungeonGenerator.cs
@@ -6,26 +6,18 @@ using UnityEngine.AI;
 public class DungeonGenerator : MonoBehaviour
 {
     public GameObject[] roomPrefabs;
-
     public int numberOfRooms = 10;
-
     public GameObject startRoomPrefab;
-
     public GameObject lastRoomPrefab;
-
+    public GameObject floor10BossRoom;
     public GameObject player;
-
     private List<GameObject> spawnedRooms = new List<GameObject>();
-
     public Vector3 roomOffset = new Vector3(20f, 0f, 20f);
-
     public float playerHeightOffset = 1.5f;
-
     public NavMeshSurface navMeshSurface;
-
     private int currentRoom = 0;
+    private GameObject roomPrefab;
 
-    // Start is called before the first frame update
     void Start()
     {
         GenerateDungeon();
@@ -42,33 +34,29 @@ public class DungeonGenerator : MonoBehaviour
         GameObject startRoom = Instantiate(startRoomPrefab, currentPosition, Quaternion.identity);
         spawnedRooms.Add(startRoom);
 
-        // Log first room generation
-        //Debug.Log("First room generated at position: " + currentPosition);
-
-        // Generate the middle rooms
+        // Generate middle rooms
         for (int i = 1; i < numberOfRooms; i++)
         {
-            // Choose a random room prefab
-            GameObject roomPrefab = roomPrefabs[Random.Range(0, roomPrefabs.Length)];
-
+            if(i == 10) // if floor is 10, spawn boss room
+            {
+                roomPrefab = floor10BossRoom;
+            }
+            else // else choose a random room prefab 
+            {
+                roomPrefab = roomPrefabs[Random.Range(0, roomPrefabs.Length)];
+            }
             // Determine the next room's position
             currentPosition += roomOffset;
 
             // Instantiate the room at the current position
             GameObject newRoom = Instantiate(roomPrefab, currentPosition, Quaternion.identity);
             spawnedRooms.Add(newRoom);
-
-            // Log each room generation
-            //Debug.Log("Room " + i + " generated at position: " + currentPosition);
         }
 
         // Generate the last room
         currentPosition += roomOffset;
         GameObject lastRoom = Instantiate(lastRoomPrefab, currentPosition, Quaternion.identity);
         spawnedRooms.Add(lastRoom);
-
-        // Log last room generation
-        //Debug.Log("Last room generated at position: " + currentPosition);
     }
 
     void TeleportPlayerToFirstRoom()
@@ -80,9 +68,6 @@ public class DungeonGenerator : MonoBehaviour
             // Apply height offset to the player's Y position
             Vector3 playerTargetPosition = new Vector3(firstRoomPosition.x, firstRoomPosition.y + playerHeightOffset, firstRoomPosition.z);
 
-            // Debug log to check teleportation
-            //Debug.Log("Teleporting player to: " + playerTargetPosition);
-
             player.transform.SetPositionAndRotation(playerTargetPosition, Quaternion.identity);
         }
         else
@@ -93,21 +78,24 @@ public class DungeonGenerator : MonoBehaviour
 
     public void TeleportPlayerToNextRoom() 
     {
-        if(currentRoom + 1 < spawnedRooms.Count) {
+        if(currentRoom + 1 < spawnedRooms.Count) 
+        {
             currentRoom++;
-            if(currentRoom >= 0 && currentRoom < spawnedRooms.Count) {
+            if(currentRoom >= 0 && currentRoom < spawnedRooms.Count) 
+            {
                 Vector3 targetRoomPosition = spawnedRooms[currentRoom].transform.position;
                 Vector3 playerTargetPosition;
-                Debug.Log("currentroom index: " + currentRoom);
-                if(spawnedRooms[currentRoom].tag == "diagonal") {
+
+                if(spawnedRooms[currentRoom].tag == "diagonal") // correct teleport positioning for diagonal room
+                {
                     playerTargetPosition = new Vector3(targetRoomPosition.x - 4, targetRoomPosition.y - 6 + playerHeightOffset, targetRoomPosition.z - 8);
-                    Debug.Log("Teleporting player to diagonal room" + playerTargetPosition);
                 }
-                else {
+                else // regular teleport position
+                {
                     playerTargetPosition = new Vector3(targetRoomPosition.x, targetRoomPosition.y + playerHeightOffset, targetRoomPosition.z);
-                    Debug.Log("Teleporting player to next room" + playerTargetPosition);
 
                 }
+
                 CharacterController cc = player.GetComponent<CharacterController>();
                 if (cc != null)
                 {
